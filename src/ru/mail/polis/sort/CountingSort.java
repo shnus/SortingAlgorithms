d3035eddc00f1834249d1b266a8c8fea04eeb28b
Nusrat Shakarov <nusrat558@gmail.com>
2017-11-28 01:46:20 +0300

    without benchmarks

---
+++ src/ru/mail/polis/sort/CountingSort.java
@@ -0,0 +1,32 @@
+package ru.mail.polis.sort;
+
+import ru.mail.polis.structures.IntKeyObject;
+
+public class CountingSort {
+    public <T extends IntKeyObject> void sort(T[] a) {
+        int max = findMax(a);
+        int[] count = new int[max + 1];
+        for (T x : a) count[x.getKey()]++;
+
+        for (int i = 1; i <= max; i++) {
+            count[i] += count[i - 1];
+        }
+        T[] res = (T[]) new IntKeyObject[a.length];
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            res[--count[a[i].getKey()]] = a[i];
+        }
+
+        System.arraycopy(res, 0, a, 0, a.length);
+    }
+
+    private <T extends IntKeyObject> int findMax(T[] a) {
+        int max = 0;
+        for (T x : a) {
+            max = Math.max(max, x.getKey());
+        }
+        return max;
+    }
+
+
+}
--- tests/ru/mail/polis/sort/valid/SimpleTest.java
+++ tests/ru/mail/polis/sort/valid/SimpleTest.java
@@ -1,11 +1,13 @@
 package ru.mail.polis.sort.valid;
 
-import java.io.IOException;
-
 import org.junit.Assert;
 import org.junit.Test;
+import ru.mail.polis.sort.*;
+import ru.mail.polis.structures.IntKeyObject;
+import ru.mail.polis.structures.IntKeyStringValueObject;
+import ru.mail.polis.structures.SimpleInteger;
 
-import ru.mail.polis.sort.SimpleSortOnComparisons;
+import java.io.IOException;
 
 /**
  * Created by Nechaev Mikhail
@@ -19,5 +21,97 @@ public class SimpleTest {
         String[] array = new String[]{"abc", "bcd", "cde", "acd", "zxy", "bba"};
         simpleSort.sort(array);
         Assert.assertArrayEquals(new String[]{"abc", "acd", "bba", "bcd", "cde", "zxy"}, array);
+    }
+
+    @Test
+    public void testInsertionSort01() throws IOException {
+        InsertionSort insertionSort = new InsertionSort();
+        String[] array = new String[]{"abc", "bcd", "cde", "acd", "zxy", "bba"};
+        insertionSort.sort(array);
+        Assert.assertArrayEquals(new String[]{"abc", "acd", "bba", "bcd", "cde", "zxy"}, array);
+    }
+
+    @Test
+    public void testQuickSort01() {
+        QuickSort quickSort = new QuickSort();
+        String[] array = new String[]{"abc", "bcd", "cde", "acd", "zxy", "bba"};
+        quickSort.sort(array);
+        Assert.assertArrayEquals(new String[]{"abc", "acd", "bba", "bcd", "cde", "zxy"}, array);
+    }
+
+    @Test
+    public void testQuickSortOptim01() {
+        QuickSortOptim quickSort = new QuickSortOptim();
+        String[] array = new String[]{"abc", "bcd", "cde", "acd", "zxy", "bba"};
+        quickSort.sort(array);
+        Assert.assertArrayEquals(new String[]{"abc", "acd", "bba", "bcd", "cde", "zxy"}, array);
+    }
+
+    @Test
+    public void testQuickSortWithInsertions01() {
+        QuickSortWithInsertions quickSort = new QuickSortWithInsertions();
+        String[] array = new String[]{"abc", "bcd", "cde", "acd", "zxy", "bba"};
+        quickSort.sort(array);
+        Assert.assertArrayEquals(new String[]{"abc", "acd", "bba", "bcd", "cde", "zxy"}, array);
+    }
+
+    @Test
+    public void testMergeSort01() {
+        MergeSort mergeSort = new MergeSort();
+        String[] array = new String[]{"abc", "bcd", "cde", "acd", "zxy", "bba"};
+        mergeSort.sort(array);
+        Assert.assertArrayEquals(new String[]{"abc", "acd", "bba", "bcd", "cde", "zxy"}, array);
+    }
+
+    @Test
+    public void testHeapSort01() {
+        HeapSort heapSort = new HeapSort();
+        String[] array = new String[]{"abc", "bcd", "cde", "acd", "zxy", "bba"};
+        heapSort.sort(array);
+        Assert.assertArrayEquals(new String[]{"abc", "acd", "bba", "bcd", "cde", "zxy"}, array);
+    }
+
+    @Test
+    public void testCounterSort01() {
+        CountingSort countingSort = new CountingSort();
+        IntKeyObject obj1 = new IntKeyStringValueObject(5,"abc");
+        IntKeyObject obj2 = new IntKeyStringValueObject(3,"aba");
+        IntKeyObject obj3 = new IntKeyStringValueObject(5,"afc");
+        IntKeyObject obj4 = new IntKeyStringValueObject(1,"fbc");
+        IntKeyObject obj5 = new IntKeyStringValueObject(2,"gbc");
+        IntKeyObject obj6 = new IntKeyStringValueObject(1,"rew");
+        IntKeyObject[] array = new IntKeyObject[]{obj1, obj2, obj3, obj4, obj5, obj6};
+        countingSort.sort(array);
+        Assert.assertArrayEquals(new IntKeyObject[]{obj4, obj6, obj5, obj2, obj1, obj3}, array);
+    }
+
+    @Test
+    public void testLsdSort01() {
+        LsdSort lsdSort = new LsdSort();
+        SimpleInteger[] array = new SimpleInteger[]{
+                new SimpleInteger(54348),
+                new SimpleInteger(24645),
+                new SimpleInteger(37345),
+                new SimpleInteger(15432),
+                new SimpleInteger(93464),
+                new SimpleInteger(56364),
+                new SimpleInteger(34656)
+        };
+        lsdSort.sort(array);
+
+        Integer[] integers = new Integer[array.length];
+        for(int i = 0; i<array.length; i++){
+            integers[i] = array[i].getInteger();
+        }
+
+        Assert.assertArrayEquals(new Integer[]{
+                new Integer(15432),
+                new Integer(24645),
+                new Integer(34656),
+                new Integer(37345),
+                new Integer(54348),
+                new Integer(56364),
+                new Integer(93464)
+        }, integers);
     }
 }
